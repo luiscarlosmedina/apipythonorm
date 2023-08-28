a7ac3bfa568dcbd990c97b64277295853f103a75
luismedina <luiscarlosmedina481@gmail.com>
2023-08-28 15:10:44 -0500

    clase v2

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Cancion
+from .modelos import db, Cancion, Albun, Usuario
 
 app = create_app("default")
 app_context = app.app_context()
@@ -9,6 +9,10 @@ db.init_app(app)
 db.create_all()
 with app.app_context():
     c =Cancion(titulo='prueba', minutos =2, segundos=25, interprete='artista')
+    a = Albun(titulo='prueba', anho=2012, descripcion='origen puertoriqueño', medio="2")
+    u = Usuario(nombre="luiscarlos", contrasenha="adminluis")
     db.session.add(c)
+    db.session.add(a)
+    db.session.add(u)
     db.session.commit()
-    print(Cancion.query.all())
+    print(Cancion.query.all(), Albun.query.all(), Usuario.query.all())
--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
@@ -11,3 +12,26 @@ class Cancion(db.Model):
 
     def __repr__(self):
         return "{}-{}-{}-{}".format(self.titulo, self.minutos, self.segundos, self.interprete)
+
+class Medio(enum.Enum):
+    DISCO = 1
+    CASETE = 2
+    CD = 3
+class Albun(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    anho = db.Column(db.Integer)
+    descripcion = db.Column(db.String(100))
+    medio = db.Column(db.Enum(Medio))
+
+    def __repr__(self):
+        return "{}-{}-{}-{}".format(self.titulo, self.anho, self.descripcion, self.medio)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre= db.Column(db.String(128))
+    contrasenha = db.Column(db.String(128))
+
+    def __repr__(self):
+        return "{}-{}".format(self.nombre, self.contrasenha)
+
