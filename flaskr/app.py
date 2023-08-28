9bfe26eaa0efa0e9ce6ba1cf3cfa0d893a52e2f5
luismedina <luiscarlosmedina481@gmail.com>
2023-08-28 14:44:40 -0500

    clase v1

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,14 @@
 from flaskr import create_app
+from .modelos import db, Cancion
 
 app = create_app("default")
 app_context = app.app_context()
 app_context.push()
+
+db.init_app(app)
+db.create_all()
+with app.app_context():
+    c =Cancion(titulo='prueba', minutos =2, segundos=25, interprete='artista')
+    db.session.add(c)
+    db.session.commit()
+    print(Cancion.query.all())
